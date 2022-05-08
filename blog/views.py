25f537fd11a6c079620bf44aef131d13d87b27aa
sangyeop <lyc2004y@gmail.com>
2022-05-09 01:14:15 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,14 @@ def index(request):
             'posts':posts
         }
     )
+
+def single_post_page(requests, pk):
+    post = Post.objectsget(pk = pk)
+
+    return render(
+        requests,
+        'blog/single_post_page.html',
+        {
+            'post':post
+        }
+    )
